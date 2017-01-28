09c92dfb0bf980e25c2ea84da491e62c94d8d600
Lisa Williams <williams.lisarenee@gmail.com>
2017-01-27 23:24:32 -0500

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, date) VALUES ('Son of a Bun', now());
+INSERT INTO burgers (burger_name, date) VALUES ('Double Whatta-Burger with Cheese', now());
+INSERT INTO burgers (burger_name, date) VALUES ('Buffalo Soldier', now());
+INSERT INTO burgers (burger_name, date) VALUES ('Cheeseburger in Paradise ', now());
